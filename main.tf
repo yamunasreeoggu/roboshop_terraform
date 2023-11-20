84dc406850263eea0eb82b0f0144190b43cd999e
yamunasreeoggu <yamunasree321@gmail.com>
2023-11-20 13:09:03 -0500

    D75 | Yamuna| corrted code for vpc

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ module "vpc" {
   vpc_cidr               = var.vpc_cidr
   env                    = var.env
   public_subnets         = var.public_subnets
-  private_subnets        = var.web_subnets
-  private_subnets        = var.app_subnets
-  private_subnets        = var.db_subnets
+  web_subnets            = var.web_subnets
+  app_subnets            = var.app_subnets
+  db_subnets             = var.db_subnets
   azs                    = var.azs
   account_no             = var.account_no
   default_vpc_id         = var.default_vpc_id
